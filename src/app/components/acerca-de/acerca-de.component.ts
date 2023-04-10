869f48ba75efb75f563d09ff3dda9399cf91ef38
FederiKopp <kopp.luisd@gmail.com>
2023-04-10 14:08:21 -0300

    intentando tomar datos desde el json

--- src/app/components/acerca-de/acerca-de.component.ts
+++ src/app/components/acerca-de/acerca-de.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AcercaDeService } from 'src/app/service/acerca-de.service';
 
 @Component({
   selector: 'app-acerca-de',
   templateUrl: './acerca-de.component.html',
   styleUrls: ['./acerca-de.component.css']
 })
-export class AcercaDeComponent {
 
+export class AcercaDeComponent implements OnInit {
+
+  AcercaDe: any;
+
+  constructor(private acercaDeService: AcercaDeService) { }
+
+  ngOnInit(): void {
+    this.acercaDeService.getAcercaDe().subscribe(data => {
+      this.acercaDeService = data;
+    });
+  }
 }
